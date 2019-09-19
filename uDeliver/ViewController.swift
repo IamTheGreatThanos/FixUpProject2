9edb01a9532d2523427700d10fc5d543b109a511
Dinmukhammed <iamthegreatthanos@gmail.com>
2019-09-19 16:38:33 +0600

    Initial Commit

---
+++ uDeliver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uDeliver
+//
+//  Created by Mac Book on 9/19/19.
+//  Copyright © 2019 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
